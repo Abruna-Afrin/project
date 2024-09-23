e4c35622244beeec397f230cf040d6d0b702d3b7
Yousuf Hossain <yousuf_hossain14@hotmail.com>
2024-09-23 11:28:20 -0400

    Added changes to controller, model and reposetory to make the code path work

--- src/api/matrimony/app/api/v1/UserController.py
+++ src/api/matrimony/app/api/v1/UserController.py
@@ -1,8 +1,13 @@
+import logging
+
 from app.models.createUser import UserCreate
 from fastapi import APIRouter, HTTPException
 from app.services.userService import create_user
 
 router = APIRouter()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @router.post("/")
 async def create_user_endpoint(user: UserCreate):
@@ -10,4 +15,5 @@ async def create_user_endpoint(user: UserCreate):
         user_id = await create_user(user)
         return {"user_id": user_id}
     except Exception as e:
+        logger.exception("Error creating user")
         raise HTTPException(status_code=400, detail=str(e))
